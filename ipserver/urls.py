7cc4a437b4b68cb2c5b3e42f38866679a5b7218d
qiaoyuanzhen <narutozb@hotmail.com>
2024-10-20 21:51:36 +0900

    添加新的测试方法

--- ipserver/urls.py
+++ ipserver/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import get_client_ip
+from .views import get_client_ip, get_client_meta_data
 
 urlpatterns = [
     path('', get_client_ip),
-    path('meta/', get_client_ip),
+    path('meta/', get_client_meta_data),
 ]
